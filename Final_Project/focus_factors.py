4d2a86aa41eb558d54293cb23e982ef33a5b8d30
raskong <s193984@dtu.dk>
2024-04-30 10:27:08 +0200

    igen

--- Final_Project/focus_factors.py
+++ Final_Project/focus_factors.py
@@ -15,8 +15,9 @@ df = df[df['CONTRIBUTING FACTOR VEHICLE 1'].isin(focus_factors)]
 
 
 #%%Focus counts:
-counts = df['CONTRIBUTING FACTOR VEHICLE 1'].value_counts()
+counts = df['CONTRIBUTING FACTOR VEHICLE 1'].value_counts().head(20)
 
+plt.figure(figsize=(8, 5))
 plt.bar(counts.index, counts)
 #plt.yscale('log')
 
@@ -24,7 +25,7 @@ plt.bar(counts.index, counts)
 plt.xlabel('Focus factor')
 plt.xticks(rotation = 90)
 plt.ylabel('Count')
-
+plt.title('Counts per contributing factor for vehicle 1')
 plt.savefig('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/10. semester/Dataanalyse/Gitpage/raskong.github.io/Final_Project/figures/factor_counts.png', bbox_inches='tight')
 # Show the plot
 plt.show()
@@ -201,8 +202,8 @@ normalized_crimes = weekday_factors.div(weekday_factors.sum(axis=0), axis=1)
 normalized_crimes.index = normalized_crimes.index.astype(str)
 
 output_notebook()
-df_bokeh = ColumnDataSource(normalized_crimes)
-#df_bokeh = ColumnDataSource(weekday_factors)
+#df_bokeh = ColumnDataSource(normalized_crimes)
+df_bokeh = ColumnDataSource(weekday_factors)
 
 weekdays = [str(i) for i in range(7)]
 colors = sns.color_palette("husl", len(focus_factors))
@@ -233,7 +234,42 @@ output_file('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/1
 show(p) #displays your plot
 
 
-#%% Bokeh year
+#%% Bokeh year not normalized
+output_notebook()
+normalized_crashes = year_factors.div(year_factors.sum(axis=0), axis=1)
+normalized_crashes.index = normalized_crashes.index.astype(str)
+
+year_factors.index = year_factors.index.astype(str)
+
+#df_bokeh = ColumnDataSource(normalized_crashes)
+df_bokeh = ColumnDataSource(year_factors)
+
+years = [str(2013 + i) for i in range(11)]
+colors = sns.color_palette("husl", len(focus_factors))
+colors = ['#%02x%02x%02x' % (int(r * 255), int(g * 255), int(b * 255)) for r, g, b in colors]
+
+p = figure(x_range = FactorRange(factors=years), title="Crashes per year", x_axis_label='Year', y_axis_label='Crashes', height=400, width=700) 
+k = 0
+
+bar ={}
+items = []
+for indx,i in enumerate(focus_factors):
+    bar[i] = p.vbar(x='YEAR',  top=i, source = df_bokeh, width=0.7,
+                      muted_alpha=0.05, muted = True, fill_color = colors[indx])
+
+     ### for the custom legend // you need to figure out where to add it
+    items.append((i, [bar[i]])) ### figure where to add it
+    legend = Legend(items=items, location=(0, 0)) ## figure where to add it
+
+
+p.add_layout(legend, 'left')
+p.legend.click_policy="mute" ### assigns the click policy (you can try to use ''hide'
+
+
+output_file('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/10. semester/Dataanalyse/Gitpage/raskong.github.io/Final_Project/Figures/bokeh_year.html')
+show(p) #displays your plot
+
+#%% Bokeh year normalized
 output_notebook()
 normalized_crashes = year_factors.div(year_factors.sum(axis=0), axis=1)
 normalized_crashes.index = normalized_crashes.index.astype(str)
@@ -265,7 +301,7 @@ p.add_layout(legend, 'left')
 p.legend.click_policy="mute" ### assigns the click policy (you can try to use ''hide'
 
 
-output_file('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/10. semester/Dataanalyse/Gitpage/raskong.github.io/Final_Project/Figures/bokeh_year.html')
+output_file('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/10. semester/Dataanalyse/Gitpage/raskong.github.io/Final_Project/Figures/bokeh_year_norm.html')
 show(p) #displays your plot
 
 
@@ -286,7 +322,26 @@ plt.savefig('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/1
 # Show the plot
 plt.show()
 
-#%%
+
+
+# %%#%%Car type counts:
+df.loc[df['VEHICLE TYPE CODE 1'] == 'sport utility / station wagon', 'VEHICLE TYPE CODE 1'] = 'station wagon/sport utility vehicle'
+focus_vehicles = df['VEHICLE TYPE CODE 1'].value_counts().head(10).index
+counts_vehicle = df['VEHICLE TYPE CODE 1'].value_counts().head(10)
+
+plt.bar(counts_vehicle.index, counts_vehicle)
+#plt.yscale('log')
+
+# Label the axes
+plt.xlabel('Focus factor')
+plt.xticks(rotation = 90)
+plt.ylabel('Count')
+
+plt.savefig('/Users/rasmuskongsted/Documents/Danmarks Tekniske Universitet/DTU/10. semester/Dataanalyse/Gitpage/raskong.github.io/Final_Project/figures/cartype_counts.png')
+# Show the plot
+plt.show()
+
+
 #%%killed per car
 #sedan
 sedan = df[df['VEHICLE TYPE CODE 1']=='sedan']
